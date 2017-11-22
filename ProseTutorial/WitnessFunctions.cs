9972c687a8b3a3e9c92f0f7e1ef148af69efa8d2
Gustavo Soares <gsoares@microsoft.com>
2017-11-22 09:52:34 -0800

    adding comments to the witness functions

--- ProseTutorial/WitnessFunctions.cs
+++ ProseTutorial/WitnessFunctions.cs
@@ -23,6 +23,15 @@ namespace ProseTutorial
     new Regex(@"$")  // End of line
 };
 
+
+        /// <summary>
+        /// A witness function is defined for one parameter of an operator, not the entire operator.
+        /// This witness function  deduces a specification on the second parameter (index 1) of the Substring operator 
+        /// given a specification on the entire Substring operator
+        /// </summary>
+        /// <param name="rule">The Substring operator's rule</param>
+        /// <param name="spec">The specification for the Substring operator</param>
+        /// <returns></returns>
         [WitnessFunction(nameof(Semantics.Substring), 1)]
         public ExampleSpec WitnessStartPosition(GrammarRule rule, ExampleSpec spec)
         {
@@ -30,14 +39,19 @@ namespace ProseTutorial
             //Each example is a map from an input state (State) to an output value (object)
             var result = new Dictionary<State, object>();
 
-            //iterate over the input-output examples for Substring operator and refine them to the pos operator
+            //iterate over the input-output examples for the Substring operator 
             foreach (var example in spec.Examples)
             {
+                //get the input state of the current example
                 State inputState = example.Key;
-                //Get the input string value from the input state. 
+                // the first parameter of Substring is the input variable 'v'
+                // we extract its current bound value from the given input state
+                // (e.g., "(Gustavo Soares)")
                 var input = inputState[rule.Body[0]] as string;  
-                //Get the output value
+                //Get the output value (e.g., "Gustavo Soares")
                 var output = example.Value as string;
+                //now we deduce a spec on the first position of the substring
+                //In this tutorial, for simplification, we consider just the first ocurrence of the substring
                 var refinedExample = input.IndexOf(output);
                 result[inputState] = refinedExample; 
             }
@@ -53,6 +67,7 @@ namespace ProseTutorial
                 State inputState = example.Key;
                 var input = inputState[rule.Body[0]] as string;
                 var output = example.Value as string;
+                //similar to the previous witness fnction, we deduce a spec on the second position of the substring
                 var refinedExample = input.IndexOf(output) + output.Length;
                 result[inputState] = refinedExample;
             }
