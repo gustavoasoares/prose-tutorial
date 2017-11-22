f2665c32065af772c3ff95433fcdaea27ad504fe
Gustavo Soares <gsoares@microsoft.com>
2017-11-22 10:35:55 -0800

    making code more readble

--- ProseTutorial/WitnessFunctions.cs
+++ ProseTutorial/WitnessFunctions.cs
@@ -18,17 +18,12 @@ namespace ProseTutorial
         [WitnessFunction(nameof(Semantics.Substring), 1)]
         public ExampleSpec WitnessStartPosition(GrammarRule rule, ExampleSpec spec)
         {
-            //a result of a witness function is a refined example-based specication 
-            //Each example is a map from an input state (State) to an output value (object)
             var result = new Dictionary<State, object>();
 
-            //iterate over the input-output examples for Substring operator and refine them to the pos operator
             foreach (var example in spec.Examples)
             {
                 State inputState = example.Key;
-                //Get the input string value from the input state. 
                 var input = inputState[rule.Body[0]] as string;  
-                //Get the output value
                 var output = example.Value as string;
                 var refinedExample = input.IndexOf(output);
                 result[inputState] = refinedExample; 
