f686d601e8a984c375bdff8e1c554477005271e2
Gustavo Soares <gsoares@microsoft.com>
2017-11-23 00:15:10 -0800

    updating tests

--- ProseTutorial.Tests/SubstringTest.cs
+++ ProseTutorial.Tests/SubstringTest.cs
@@ -10,22 +10,19 @@ using Microsoft.ProgramSynthesis.AST;
 using Microsoft.ProgramSynthesis.Learning.Strategies;
 using Microsoft.ProgramSynthesis.Learning.Logging;
 
-namespace ProseTutorial
-{
+namespace ProseTutorial {
     [TestClass]
-    public class SubstringTest
-    {
+    public class SubstringTest {
         [TestMethod]
-        public void TestLearnSubstringPositiveAbsPos()
-        {
+        public void TestLearnSubstringPositiveAbsPos() {
             //set up the grammar 
             var grammar = DSLCompiler.
                 ParseGrammarFromFile("../../../ProseTutorial/grammar/substring.grammar");
             var prose = ConfigureSynthesis(grammar.Value);
 
             //create the example
-            var input = State.CreateForExecution(grammar.Value.InputSymbol, "(Gustavo Soares)");
-            var examples = new Dictionary<State, object> { { input, "Gustavo Soares" } };
+            var input = State.CreateForExecution(grammar.Value.InputSymbol, "19-Feb-1960");
+            var examples = new Dictionary<State, object> { { input, "Feb" } };
             var spec = new ExampleSpec(examples);
 
             //learn the set of programs that satisfy the spec 
@@ -35,7 +32,7 @@ namespace ProseTutorial
             //the output is correct
             var programs = learnedSet.RealizedPrograms;
             var output = programs.First().Invoke(input) as string;
-            Assert.AreEqual("Gustavo Soares", output);
+            Assert.AreEqual("Feb", output);
         }
 
         [TestMethod]
@@ -46,9 +43,9 @@ namespace ProseTutorial
             var prose = ConfigureSynthesis(grammar.Value);
 
             //create the example
-            var firstInput = State.CreateForExecution(grammar.Value.InputSymbol, "5.5");
-            var secondInput = State.CreateForExecution(grammar.Value.InputSymbol, "4.3");
-            var examples = new Dictionary<State, object> { { firstInput, "5" }, { secondInput, "3" } };
+            var firstInput = State.CreateForExecution(grammar.Value.InputSymbol, "16-Feb-2016");
+            var secondInput = State.CreateForExecution(grammar.Value.InputSymbol, "14-Jan-2012");
+            var examples = new Dictionary<State, object> { { firstInput, "16" }, { secondInput, "12" } };
             var spec = new ExampleSpec(examples);
 
             //learn the set of programs that satisfy the spec 
@@ -58,9 +55,9 @@ namespace ProseTutorial
             //the output is correct
             var programs = learnedSet.RealizedPrograms;
             var output = programs.First().Invoke(firstInput) as string;
-            Assert.AreEqual("a", output);
+            Assert.AreEqual("16", output);
             output = programs.First().Invoke(secondInput) as string;
-            Assert.AreEqual("b", output);
+            Assert.AreEqual("12", output);
         }
 
         [TestMethod]
@@ -73,7 +70,7 @@ namespace ProseTutorial
             //create the examples
             var firstInput = State.CreateForExecution(grammar.Value.InputSymbol, "(Gustavo Soares)");
             var secondInput = State.CreateForExecution(grammar.Value.InputSymbol, "(Titus Barik)");
-            var examples = new Dictionary<State, object> { { firstInput, "Gustavo Soares"}, { secondInput, "Titus Barik"}};
+            var examples = new Dictionary<State, object> { { firstInput, "Gustavo Soares" }, { secondInput, "Titus Barik" } };
             var spec = new ExampleSpec(examples);
 
             //learn the set of programs that satisfy the spec 
@@ -97,7 +94,7 @@ namespace ProseTutorial
 
             //create the example
             var firstInput = State.CreateForExecution(grammar.Value.InputSymbol, "(Gustavo Soares)");
-            var examples = new Dictionary<State, object> { { firstInput, "Gustavo Soares" }};
+            var examples = new Dictionary<State, object> { { firstInput, "Gustavo Soares" } };
             var spec = new ExampleSpec(examples);
 
             //learn the set of programs that satisfy the spec 
@@ -117,8 +114,7 @@ namespace ProseTutorial
         }
 
         [TestMethod]
-        public void TestLearnSubstringTwoExamples()
-        {
+        public void TestLearnSubstringTwoExamples() {
             var grammar = DSLCompiler.
                 ParseGrammarFromFile("../../../ProseTutorial/grammar/substring.grammar");
             var prose = ConfigureSynthesis(grammar.Value);
@@ -137,14 +133,13 @@ namespace ProseTutorial
         }
 
         [TestMethod]
-        public void TestLearnSubstringOneExample()
-        {
+        public void TestLearnSubstringOneExample() {
             var grammar = DSLCompiler.
                 ParseGrammarFromFile("../../../ProseTutorial/grammar/substring.grammar");
             var prose = ConfigureSynthesis(grammar.Value);
 
             var input = State.CreateForExecution(grammar.Value.InputSymbol, "Gustavo Soares");
-            var examples = new Dictionary<State, object> { { input, "Soares" }};
+            var examples = new Dictionary<State, object> { { input, "Soares" } };
 
             var spec = new ExampleSpec(examples);
 
@@ -159,8 +154,7 @@ namespace ProseTutorial
             Assert.AreEqual("Gulwani", output2);
         }
 
-        public static SynthesisEngine ConfigureSynthesis(Grammar grammar)
-        {
+        public static SynthesisEngine ConfigureSynthesis(Grammar grammar) {
             var witnessFunctions = new WitnessFunctions(grammar);
             var deductiveSynthesis = new DeductiveSynthesis(witnessFunctions);
             var synthesisExtrategies = new ISynthesisStrategy[] { deductiveSynthesis };
