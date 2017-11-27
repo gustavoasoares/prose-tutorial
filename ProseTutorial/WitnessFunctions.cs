8fd19fcb1631584cd273803ef91cc6ee79973408
Gustavo Soares <gsoares@microsoft.com>
2017-11-27 14:58:53 -0800

    fixing comments

--- ProseTutorial/WitnessFunctions.cs
+++ ProseTutorial/WitnessFunctions.cs
@@ -46,14 +46,6 @@ namespace ProseTutorial {
             return new ExampleSpec(result);
         }
 
-        /// <summary>
-        /// This witness function should deduce the spec for k given the spec for AbsPos     
-        /// </summary>
-        /// <param name="rule"></param>
-        /// <param name="spec"></param>
-        /// <returns>However, now we need to produce two possible specs for k (positive and negative)
-        /// given a single spec for AbsPos. A disjunction of possible specs has its own 
-        /// representative spec type in PROSE – DisjunctiveExamplesSpec.</returns>
         [WitnessFunction(nameof(Semantics.AbsPos), 1)]
         public DisjunctiveExamplesSpec WitnessK(GrammarRule rule, DisjunctiveExamplesSpec spec) {
             var kExamples = new Dictionary<State, IEnumerable<object>>();
@@ -63,11 +55,7 @@ namespace ProseTutorial {
 
                 var positions = new List<int>();
                 foreach (int pos in example.Value) {
-                    //the positive spec for k
                     positions.Add(pos + 1);
-                    //TODO add the negative spec for k 
-                    //uncomment the next statement and replace X by the expression that should return the negative spec
-                    //positions.Add(X);
                 }
                 if (positions.Count == 0) return null;
                 kExamples[inputState] = positions.Cast<object>();
