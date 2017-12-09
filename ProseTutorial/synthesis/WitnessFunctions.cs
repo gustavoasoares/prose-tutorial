c2a9a32a238601a0765c9f219e97bc30def0e296
Gustavo Soares <gsoares@microsoft.com>
2017-12-08 16:09:26 -0800

    vscode

--- ProseTutorial/synthesis/WitnessFunctions.cs
+++ ProseTutorial/synthesis/WitnessFunctions.cs
@@ -23,7 +23,6 @@ namespace ProseTutorial {
                 var output = example.Value as string;
                 var occurrences = new List<int>();
 
-                occurrences.Add(input.IndexOf(output));
                 for (int i = input.IndexOf(output); i >= 0; i = input.IndexOf(output, i + 1)) {
                     occurrences.Add(i);
                 }
@@ -35,25 +34,14 @@ namespace ProseTutorial {
 
         }
 
-        /// <summary>
-        /// This is a conditional witness function. In the presence of multiple occurrences of the output string in the input string,
-        /// We can only write a witness function for an end position for each start position. Thus, our witness function for the end position
-        /// is conditional on the start position: in addition to an outer spec, it takes an additional input – a spec on its prerequisite start position parameter.
-        /// </summary>
-        /// <param name="rule"></param>
-        /// <param name="spec"></param>
-        /// <param name="startSpec">The specification of the start position parameter</param>
-        /// <returns></returns>
         [WitnessFunction(nameof(Semantics.Substring), 2, DependsOnParameters = new []{1})]
         public ExampleSpec WitnessEndPosition(GrammarRule rule, ExampleSpec spec, ExampleSpec startSpec) {
             var result = new Dictionary<State, object>();
             foreach (var example in spec.Examples) {
                 State inputState = example.Key;
                 var output = example.Value as string;
-                //get the spec on the start position 
-                var start = (int)startSpec.Examples[inputState];
-                //TODO given the spec on the start position, write a spec on the end position
-                //result[inputState] = ...;
+                var start = (int) startSpec.Examples[inputState];
+                result[inputState] = start + output.Length;
             }
             return new ExampleSpec(result);
         }
